d76f3303566930a26f3ab9f5df74fd7bc8065955
Tapabrata Dey <tapabratadey02@gmail.com>
2021-05-15 19:58:08 -0400

    Simple Stock Price App #learningDS

---
+++ stockPrice.py
@@ -0,0 +1,18 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write("""
+# Simple Stock Price App
+
+Shown are the stock *closing price* and volume of Tesla!
+""")
+
+tickerSymbol = 'TSLA'
+
+tickerData = yf.Ticker(tickerSymbol)
+
+tickerDf = tickerData.history(period="1d", start='2010-05-31', end='2020-05-31')
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
